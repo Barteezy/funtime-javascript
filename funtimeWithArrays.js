caf2e8efbb0862ea769ea60893390c28dce003a1
Andrew Jaswa <ajaswa@gmail.com>
2015-06-17 14:40:55 -0600

    arrays grader

---
+++ funtimeWithArrays.js
@@ -0,0 +1,16 @@
+/* Given:
+ * var dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"];
+ * Implement: 
+ *
+ * howManyDogs(dogs);
+ * // => 6
+ *
+ * nameLengths(dogs)
+ * // => [4, 8, 4, 6, 4, 7]
+ *
+ * reverseDogNames(dogs);
+ * // => ['odiF', 'hgielraH', 'ilaM', 'eixirT', 'wonS', 'yrotciV']
+ *
+ * firstThreeDogsWithEach(dogs);
+ * //=>  ["Fido", "Harleigh", "Mali"]
+ */
---
+++ letterGrader.js
@@ -0,0 +1,6 @@
+/* Given an array of test scores [ 68, 74, 99, 45, 83, 95 ] and the scale: F is less than 60, D is 60 to 69, C is 70 to 79, B is 80 to 89, A is 90 to 100.
+ * Write a function that returns an array
+ * Example:
+ * convertToGrade(arr); 
+ * // => [ D, C, A, F, B, A ]
+ */
